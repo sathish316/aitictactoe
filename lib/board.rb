a92b81a01aeafd82124c79518f8bac0ec85333c4
Sathish <sathish316@gmail.com>
2011-12-27 19:39:50 +0530

    unoptimized gameover and winner check

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,8 @@
 class Board
-  def initialize
-    @board = ["...","...","..."]
+  EMPTY = '.'
+  
+  def initialize(board=["...","...","..."])
+    @board = board.map {|row| row.chars.to_a}
   end
   
   def mark(x,y,player)
@@ -12,6 +14,53 @@ class Board
   end
   
   def draw
-    @board.join("\n")
+    output = ""
+    3.times do |i|
+      3.times do |j|
+        output << @board[i][j]
+      end
+      output << "\n"
+    end
+    output
+  end
+  
+  def game_over?
+    all_x = ['X','X','X']
+    all_o = ['O','O','O']
+    all_rows = rows
+    all_rows.any?{|row| row == all_x} || all_rows.any?{|row| row == all_o} || !has_empty_position?
+  end
+  
+  def winner
+    all_rows = rows
+    all_x = ['X','X','X']
+    all_o = ['O','O','O']
+    return 'X' if all_rows.any?{|row| row == all_x}
+    return 'O' if all_rows.any?{|row| row == all_o}
+  end
+  
+  private
+  
+  def has_empty_position?
+    @board.flatten.include?(EMPTY)
+  end
+  
+  def rows
+    horizontals + verticals + diagonals
+  end
+  
+  def horizontals
+    @board
+  end
+  
+  def verticals
+    @board.transpose
+  end
+  
+  def diagonals
+    [
+      [@board[0][0], @board[1][1], @board[2][2]],
+      [@board[0][2], @board[1][1], @board[2][0]],
+    ]
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,7 +9,7 @@ describe Board do
 ...
 BOARD
       board = Board.new
-      board.draw.should == board_output.chop
+      board.draw.should == board_output
     end
 
     it "should draw board position with players" do
@@ -21,7 +21,7 @@ BOARD
       board = Board.new
       board.mark(1,2,Player.new('X',nil))
       board.mark(2,1,Player.new('O',nil))
-      board.draw.should == board_output.chop
+      board.draw.should == board_output
     end
   end
   
@@ -34,4 +34,55 @@ BOARD
     board.get(2,1).should == 'O'
   end
 
+  describe "game over?" do
+    it "should be true if there are no more positions to play" do
+      board = Board.new([
+        "XXO",
+        "OXX",
+        "XOO"
+        ])
+      board.should be_game_over
+      board.winner.should be_nil
+    end
+    
+    it "should be true if player marks 3 columns horizontally" do
+      board = Board.new([
+        "...",
+        "XXX",
+        "..."
+        ])
+      board.should be_game_over
+      board.winner.should == 'X'
+    end
+
+    it "should be true if player marks 3 columns vertically" do
+      board = Board.new([
+        "..X",
+        "..X",
+        "..X"
+        ])
+      board.should be_game_over
+      board.winner.should == 'X'
+    end
+
+    it "should be true if player marks 3 columns diagonally" do
+      board = Board.new([
+        "..O",
+        ".O.",
+        "O.."
+        ])
+      board.should be_game_over
+      board.winner.should == 'O'
+    end
+
+    it "should be false if there are positions to play and no 3 consecutive marks are present" do
+      board = Board.new([
+        "...",
+        ".X.",
+        "X.."
+        ])
+      board.should_not be_game_over
+      board.winner.should be_nil
+    end
+  end
 end
